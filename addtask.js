fe69e697fb19408f62ef79b56f88e637b0c8fbc0
Davfernandes <davifernandes868@gmail.com>
2025-04-08 15:04:39 -0300

    Reza e Testa

--- addtask.js
+++ addtask.js
@@ -1,16 +1,15 @@
 function adicionarTarefa() {
-    const input = document.getElementById('novaTarefa');
-    const textoTarefa = input.value.trim();
-  
-    if (textoTarefa === '') {
-      alert('Por favor, digite uma tarefa.');
-      return;
-    }
-  
-    const li = document.createElement('li');
-    li.textContent = textoTarefa;
-  
-    document.getElementById('listaTarefas').appendChild(li);
-    input.value = '';
+  const input = document.getElementById('novaTarefa');
+  const textoTarefa = input.value.trim();
+
+  if (textoTarefa === '') {
+    alert('Por favor, digite uma tarefa.');
+    return;
   }
-  
+
+  const li = document.createElement('li');
+  li.textContent = textoTarefa;
+
+  document.getElementById('listaTarefas').appendChild(li);
+  input.value = '';
+}
